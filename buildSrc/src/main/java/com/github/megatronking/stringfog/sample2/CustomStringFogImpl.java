5b0b16f3f0073da1b22183aa2adad82d114f80e0
MegatronKing <1256980529@qq.com>
2019-03-05 00:01:18 +0800

    init commit

---
+++ buildSrc/src/main/java/com/github/megatronking/stringfog/sample2/CustomStringFogImpl.java
@@ -0,0 +1,30 @@
+package com.github.megatronking.stringfog.sample2;
+
+import com.github.megatronking.stringfog.IStringFog;
+
+/**
+ * 自定义算法实现，此文件需要拷贝一份到buildSrc目录下面
+ *
+ * @author Sundy
+ * @since 2019/3/4 23:41
+ */
+public class CustomStringFogImpl implements IStringFog {
+
+    @Override
+    public String encrypt(String data, String key) {
+        // 自己实现加密算法，这里举例将key添加到所有String数据前面
+        return key + data;
+    }
+
+    @Override
+    public String decrypt(String data, String key) {
+        // 自己实现解密算法，这里举例将String数据前面的key移除
+        return data.replace(key, "");
+    }
+
+    @Override
+    public boolean overflow(String data, String key) {
+        return false;
+    }
+
+}
