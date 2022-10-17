004d4b60f1d47378c8101867c6b78e5375efe916
Megatron King <1256980529@qq.com>
2022-10-18 00:10:51 +0800

    update v4.0.0

--- buildSrc/src/main/java/com/github/megatronking/stringfog/sample2/CustomKeyGenerator.java
+++ buildSrc/src/main/java/com/github/megatronking/stringfog/sample2/CustomKeyGenerator.java
@@ -7,7 +7,9 @@ import java.nio.charset.StandardCharsets;
 public class CustomKeyGenerator implements IKeyGenerator {
 
     @Override
-    public byte[] generate(String s) {
+    public byte[] generate(String value) {
+        // value是待加密字符串，这里返回加密的密钥。
+        // 示例表示使用"Hello World"作为密钥，这里可以根据不同的字符串使用不同的密钥。
         return "Hello World".getBytes(StandardCharsets.UTF_8);
     }
 
